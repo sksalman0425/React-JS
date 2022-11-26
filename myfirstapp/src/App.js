b517fb234df2c7423456578dca60f7b966bc7ca6
Salman Shaikh <sk.salman0425@gmail.com>
2022-11-26 15:47:08 +0530

    first react app uploaded

---
+++ myfirstapp/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <input type={"text"} placeholder="Enter your requirements"/>
+        <p>
+          Hello world! this is my first app in React.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
