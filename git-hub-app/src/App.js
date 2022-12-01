2f9eb6fd164706c6de25c6cdd3b7ad60e4d060c0
Salman Shaikh <sk.salman0425@gmail.com>
2022-12-01 19:05:52 +0530

    git-hub-app updated

--- git-hub-app/src/App.js
+++ git-hub-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.scss';
+import './App.css';
 
 // function App() {
 //   return (
@@ -12,38 +12,58 @@ import './App.scss';
 // }
 //we can write class component as
 
-
-	const testData = [
-			{name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
-      {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
-  		{name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
-	];
-
-class Card extends React.Component{
-  rendet(){
-    return(
-      <div className="github-profile">
-        <img src='https://dummyimage.com/75x75/000/fff'></img>
-        <div></div>
-        <div ></div>
-        ........card details
+  const testData = [
+    { name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook" },
+    { name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu" },
+    { name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook" },
+  ];
+  const CardList = (props) => {
+    return (
+      <div>
+        {
+          props.profiles.map((profile) => {
+            return <Card profile={profile} />
+          })
+        }
       </div>
     )
   }
-}
-
-
-class App extends React.Component{
-  render(){
+  const Form =(props)=>{
     return(
       <div>
-        <div className="header"> 
-           The Git Hub App 
-         </div>
-         <Card/>
-    </div>
-    );
+        <form action="">
+          <input type={"text"} placeholder="Enter github username"/>
+          <button>add Profile Card</button>
+        </form>
+      </div>
+    )
   }
-}
-
-export default App;
+  class Card extends React.Component {
+    render() {
+      const profile = this.props.profile
+      return (
+        <div style={{ margin: "1rem" }}>
+          <img src={profile.avatar_url} style={{ width: "75px" }} />
+          <div style={{ display: "inline-block", marginLeft: "12px" }}>
+            <div style={{ fontSize: "125%" }}>{profile.name}</div>
+            <div>{profile.company}</div>
+          </div>
+        </div>
+      )
+    }
+  }
+  class App extends React.Component {
+    render() {
+      return (
+        <div>
+          <div className='header'>
+            The Git Hub App
+          </div>
+          <Form/>
+          <CardList profiles={testData}/>
+        </div>
+      );
+    }
+  }
+  //profiles and profile are two different state i.e. variable.
+  export default App;
