5a49b56f2d8a823e8cdfcf3ca7ab8778791eda88
Salman Shaikh <sk.salman0425@gmail.com>
2022-12-17 10:52:51 +0530

    counterapp edited

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -9,24 +9,36 @@ import { useState } from 'react';
 function App() {
  
     const [counter, setCounter]=useState(0);
-    const handleOnIncrementClick=()=>{
-    //counter=counter+1 is wrong. we can can state value only through setter
-    console.log("Increment counter click called");
-    setCounter(counter+1);
-  };
-    const handleOnDecrementClick =()=>{
-      console.log("Decrement counter click called");
-      setCounter(counter-1);
+  //   const handleOnIncrementClick=()=>{
+  //   //counter=counter+1 is wrong. we can can state value only through setter
+  //   console.log("Increment counter click called");
+  //   setCounter(counter+1);
+  // };
+  //   const handleOnDecrementClick =()=>{
+  //     console.log("Decrement counter click called");
+  //     setCounter(counter-1);
+  //   }
+    const handleOnClick =(value)=>{
+      console.log("onclick the value of argument",value);
+      if(value==="Increment")
+      setCounter(counter+1)
+      else
+      setCounter(counter-1)
+
     }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <button onClick={handleOnIncrementClick}> Counter Increment</button>
+
+          <button onClick={()=>handleOnClick('Increment')}>Counter Increment</button>
+          <button onClick={()=>handleOnClick('Decrement')}>Counter Decrement</button>
+          {/* <button onClick={handleOnIncrementClick}> Counter Increment</button>
           
-          <button onClick={handleOnDecrementClick}> Counter Decrement </button><br/><br/>
-          {counter} 
+          <button onClick={handleOnDecrementClick}> Counter Decrement </button> */}
+          <div> {counter} </div>
+         
         </p>
         <a
           className="App-link"
