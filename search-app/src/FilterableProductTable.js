2d5ab9b36f2067a5f424ed66d75080fd7e14e501
Salman Shaikh <sk.salman0425@gmail.com>
2022-12-24 20:06:53 +0530

    changes done in search-app

--- search-app/src/FilterableProductTable.js
+++ search-app/src/FilterableProductTable.js
@@ -5,11 +5,12 @@ import { useState } from "react";
 const FilterableProductTable = ({ products }) => {
   // we can write props or other variable name directly which we have to assign
   // .i.e. here products.this is latest method which is widely used. 
-  //  console.log("Props recieved::", props);
+  //  console.log("Props recieved::::", props);
   //   const products=props.products;
   //   const { products } = props;
   //here we use usestate bz it is parent component of both searchBar and ProductTable
-  const [filterText, setFilterText]=useState("");// here initial value is null
+  const [filterText, setFilterText]=useState("");
+  // Above initial value is null
   const [inStockOnly,setInstockOnly]=useState(false);
   const onFilterTextChanged = (changedValue) => {
     console.log("Value recieved from Search bar component", changedValue);
